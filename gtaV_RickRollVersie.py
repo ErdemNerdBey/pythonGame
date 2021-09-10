81212bd2537ebf486fe0d8b79ac00f7b2197fa0c
Idioot <99065788@mydavinci.nl>
2021-09-10 17:40:37 +0200

    gtaV rickroll versie

--- gtaV_RickRollVersie.py
+++ gtaV_RickRollVersie.py
@@ -9,8 +9,27 @@ if input('om te starten zeg: never gonna give you up\n ') == 'never gonna give y
     if input('Je bent uit de gevangenis. wat ga je doen? \n 1.steel een auto . \n 2.steel een motor\n') == '1':
         print('je hebt succesfol een auto gestolen')
         if input('je rijdt op de weg. wat ga je doen? \n 1.ga sneller \n 2.rij op de zelfde snelheid\n') == '1':
-            print('je rijdt 150 km per uur. je probeert een bocht te nemen, maar je drift en flikkert van de bocht af.\n HAHA LOOOZEEERR ')
+            print('je rijdt 150 km per uur. je probeert een bocht te nemen, maar je drift en flikkert van de bocht af.\n HAHA LOOOSSEEERR ')
             time.sleep(5)
             webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
-        else: input('je bots perongeluk tegen een politie auto. Wat ga je doen?\n 1.rij weg\n 2.stop\n') == '1'
-            if input('je bent gevlucht! waar ga je heen?\n 1.naar je bunker\n 2.ga naar je huis') == '1'
+        elif input('je bots perongeluk tegen een politie auto. Wat ga je doen?\n 1.rij weg\n 2.stop\n') == '1':
+            if input('je bent gevlucht! waar ga je heen?\n 1.naar je bunker\n 2.ga naar je huis\n') == '1':
+                print('onderweg naar je bunker ben je gezien door de polite en ben je neergeschoten door de politie.\n HAHA LOOSSEEER')
+                time.sleep(5)
+                webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
+            else: print('je bent naar huis gevlucht!\n Je hebt gewonnen!!!')
+    elif print('je hebt een moter gestolen!\n je rijd op de weg.\nwat ga je doen?\n '):
+        if input('1.Ga sneller\n 2.ga met de zelfde snelheid\n') == '1':
+            print('je komt tegen auto aan en gaat dood\nHAHA LOOOSSEERR')
+            time.sleep(5)
+            webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
+        elif print('je rijdt richt jouw huisen er zijn 2 wegen. wat ga je doen?'):
+            if input('1.rij links\n 2.rij rechts\n') == '1':
+                print('je bent veilig thuis gekomen.\n je hebt gewonnen!!!')
+            else: input('je komt een serrie moordennaar tegen en wordt neergeschoten.\n HAHA LOOOSSEERR')
+            time.sleep(5)
+            webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
+            
+           
+                  
+
